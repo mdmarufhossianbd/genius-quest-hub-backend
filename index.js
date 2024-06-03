48bdc03820bb6ce49a3456ce1361a85019d1cfd9
Maruf Hossain <mdmarufh974@gmail.com>
2024-06-03 22:47:44 +0600

    user manage and contest api done

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 5000
 require('dotenv').config();
 
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ]
+}));
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nss4adm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,10 +32,11 @@ async function run() {
 
     const contestCollections = client.db('geniusQuestHub').collection('contests')
     const userCollections = client.db('geniusQuestHub').collection('users')
+    const commentCollections = client.db('geniusQuestHub').collection('comments')
 
 
 
-    // user
+    // users
     app.post('/users', async(req, res)=>{
         const user = req.body;
         const query = {email : user.email}        
@@ -48,7 +53,41 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {
+        _id : new ObjectId(id)
+      }
+      const result = await userCollections.deleteOne(query);
+      res.send(result);
+    })
 
+
+    // make creator
+    app.patch('/users/creators/:id', async(req, res)=>{ 
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const updateUserRole = {
+        $set: {
+          role: 'Creator',
+          status: {
+            block: 'Block',
+            unBlock: 'Unblock'
+          }
+        }
+      }
+      const result = await userCollections.updateOne(filter, updateUserRole);
+      res.send(result)
+    })
+
+    app.get('/users/creators/:id', async(req, res)=>{
+      const email = req.params.id;
+      const query = {email : email};
+      const user = await userCollections.findOne(query);
+      res.send(user)
+    })
+
+    // contests
     app.post('/contests', async(req, res)=>{
         const result = await contestCollections.insertOne(req.body)
         res.send(result);
@@ -59,7 +98,35 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/contests/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await contestCollections.deleteOne(query);
+      res.send(result);
+    })
 
+    app.patch('/contests/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updateStatus = {
+        $set: {
+          status : 'Publish'
+        }
+      }
+      const result = await contestCollections.updateOne(filter, updateStatus);
+      res.send(result);
+    })
+
+    // comments
+    app.post('/comments', async(req, res)=>{
+      const result = await commentCollections.insertOne(req.body)
+      res.send(result);
+    })
+
+    app.get('/comments', async(req, res)=>{
+      const result = await commentCollections.find().toArray();
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
