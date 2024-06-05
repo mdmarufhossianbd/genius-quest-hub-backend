f616ee3cac5be4f6baa5f8db5b3d33c05f670474
Maruf Hossain <mdmarufh974@gmail.com>
2024-06-06 04:15:27 +0600

    contest update api created

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(cors({
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId
+} = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nss4adm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,42 +41,51 @@ async function run() {
 
 
     // users
-    app.post('/users', async(req, res)=>{
-        const user = req.body;
-        const query = {email : user.email}        
-        const existingUser = await userCollections.findOne(query);
-        if (existingUser){
-            return res.send({message : 'This user is already exists', insertedId: null})
-        }
-        const result = await userCollections.insertOne(user);
-        res.send(result);
-    })
-
-    app.get('/users', async(req, res)=>{
-        const result = await userCollections.find(req.body).toArray();
-        res.send(result);
-    })
-
-    app.get('/users/:id', async(req, res)=>{
-      const email = req.params.id;
-      const query = {email : email}      
-      const result = await userCollections.findOne(query);      
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = {
+        email: user.email
+      }
+      const existingUser = await userCollections.findOne(query);
+      if (existingUser) {
+        return res.send({
+          message: 'This user is already exists',
+          insertedId: null
+        })
+      }
+      const result = await userCollections.insertOne(user);
       res.send(result);
     })
 
-    app.delete('/users/:id', async(req, res)=>{
+    app.get('/users', async (req, res) => {
+      const result = await userCollections.find(req.body).toArray();
+      res.send(result);
+    })
+
+    app.get('/users/:id', async (req, res) => {
+      const email = req.params.id;
+      const query = {
+        email: email
+      }
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
       const query = {
-        _id : new ObjectId(id)
+        _id: new ObjectId(id)
       }
       const result = await userCollections.deleteOne(query);
       res.send(result);
     })
 
     // user block
-    app.patch('/users/block/:id', async(req, res)=>{
+    app.patch('/users/block/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = {
+        _id: new ObjectId(id)
+      }
       const updateBlock = {
         $set: {
           status: "block"
@@ -81,11 +94,13 @@ async function run() {
       const result = await userCollections.updateOne(filter, updateBlock);
       res.send(result);
     })
-    
+
     // unblock
-    app.patch('/users/unblock/:id', async(req, res)=>{
+    app.patch('/users/unblock/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = {
+        _id: new ObjectId(id)
+      }
       const updateBlock = {
         $set: {
           status: "Unblock"
@@ -97,9 +112,11 @@ async function run() {
 
 
     // make creator
-    app.patch('/users/creators/:id', async(req, res)=>{ 
+    app.patch('/users/creators/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)};
+      const filter = {
+        _id: new ObjectId(id)
+      };
       const updateUserRole = {
         $set: {
           role: 'Creator',
@@ -117,84 +134,128 @@ async function run() {
       //   return res.status(403).send({ message: 'forbidden access' })
       // }
 
-      const query = { email: email };
+      const query = {
+        email: email
+      };
       const user = await userCollections.findOne(query)
       let admin = false;
       if (user) {
-        admin = user?.role === 'Admin';        
+        admin = user?.role === 'Admin'
       }
       res.send({admin});
     })
 
     // creator find
-    app.get('/users/creators/:email', async(req, res)=>{
+    app.get('/users/creators/:email', async (req, res) => {
       const email = req.params.email;
 
-      const query = {email : email};
+      const query = {
+        email: email
+      };
       const user = await userCollections.findOne(query);
       let creator = false;
-      if(user){
+      if (user) {
         creator = user?.role === "Creator"
       }
-      res.send({creator})
+      res.send({
+        creator
+      })
     })
 
     // contests
-    app.post('/contests', async(req, res)=>{
-        const result = await contestCollections.insertOne(req.body)
-        res.send(result);
+    app.post('/contests', async (req, res) => {
+      const result = await contestCollections.insertOne(req.body)
+      res.send(result);
     })
 
-    app.get('/contests', async(req, res)=>{
-        const result = await contestCollections.find().toArray();
-        res.send(result);
+    app.get('/contests', async (req, res) => {
+      const result = await contestCollections.find().toArray();
+      res.send(result);
     })
 
-    app.get('/contests/:id', async(req, res) =>{
+    app.get('/contests/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = {
+        _id: new ObjectId(id)
+      };
       const result = await contestCollections.findOne(query);
       res.send(result)
     })
 
-    app.get('/contests/my-contests/:email', async(req, res)=>{
+    app.get('/contests/my-contests/:email', async (req, res) => {
       const email = req.params.email;
-      const query = {creatorEmail : email}
+      const query = {
+        creatorEmail: email
+      }
       const result = await contestCollections.find(query).toArray();
       res.send(result)
-      
+
     })
 
-    app.delete('/contests/:id', async(req, res)=>{
+    app.delete('/contests/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = {
+        _id: new ObjectId(id)
+      };
       const result = await contestCollections.deleteOne(query);
       res.send(result);
     })
 
-    app.patch('/contests/:id', async(req, res)=>{
+    app.put('/contests/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = {
+        _id: new ObjectId(id)
+      };
+      const options = {
+        upsert: true
+      };
+      const updateContest = req.body;
+      const contest = {
+        $set: {
+          contestName: updateContest.contestName,
+          contestImage: updateContest.contestImage,
+          contestDescription: updateContest.contestDescription,
+          contestRegistrationFee: updateContest.contestRegistrationFee,
+          contestPrize: updateContest.contestPrize,
+          contestInstructions: updateContest.contestInstructions,
+          contestContestType: updateContest.contestContestType,
+          contestPublishDate: updateContest.contestPublishDate,
+          contestDeadline: updateContest.contestDeadline,
+        }
+      }
+      const result = await contestCollections.updateOne(filter, contest, options);
+      res.send(result);
+    })
+
+    app.patch('/contests/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {
+        _id: new ObjectId(id)
+      }
       const updateStatus = {
-            $set: {contestStatus : 'Publish'}
+        $set: {
+          contestStatus: 'Publish'
+        }
       }
       const result = await contestCollections.updateOne(filter, updateStatus);
       res.send(result);
     })
 
     // comments
-    app.post('/comments', async(req, res)=>{
+    app.post('/comments', async (req, res) => {
       const result = await commentCollections.insertOne(req.body)
       res.send(result);
     })
 
-    app.get('/comments', async(req, res)=>{
+    app.get('/comments', async (req, res) => {
       const result = await commentCollections.find().toArray();
       res.send(result)
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({
+      ping: 1
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -205,10 +266,10 @@ run().catch(console.dir);
 
 
 // server
-app.get('/', (req, res)=>{
-    res.send('Welcome to Genius Contest Hub server and it is running')
+app.get('/', (req, res) => {
+  res.send('Welcome to Genius Contest Hub server and it is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`Genius Contest Hub server is running on ${port}`);
+app.listen(port, () => {
+  console.log(`Genius Contest Hub server is running on ${port}`);
 })
